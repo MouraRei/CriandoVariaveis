984dff4a676cb8f607cbf0053414af250e18c8fa
MouraRei <reinaldomoura.contato@outlook.com>
2021-11-17 14:47:30 -0300

    Criando variaveis

---
+++ Criando Variaveis.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 29;
+        int ano = 1992;
+        Console.WriteLine("Minha idade é " + idade + ". Nasci no ano de " + ano);
+    }
+}
